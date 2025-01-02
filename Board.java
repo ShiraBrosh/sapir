ec0d754a4100208a8a86be84eea2df40c938c88c
ShiraBrosh <155542654+ShiraBrosh@users.noreply.github.com>
2025-01-02 20:50:09 +0200

    Add files via upload

--- Board.java
+++ Board.java
@@ -9,6 +9,10 @@ public class Board {
     private static final int CostGREEN = 3;
 
     private char[][] goalBoard;
+    private int lastFromI = -1;
+    private int lastFromJ = -1;
+    private int lastToI = -1;
+    private int lastToJ = -1;
 
     public Board(char[][] goalBoard) {
         this.goalBoard = goalBoard;
@@ -34,7 +38,6 @@ public class Board {
     public List<State> getNextStates(State curr) {
         List<State> nextStates = new ArrayList<>();
         char[][] board = curr.getBoard();
-
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 if (board[i][j] != '_' && board[i][j] != 'X') {
@@ -45,22 +48,41 @@ public class Board {
         return nextStates;
     }
 
+    private boolean isOppositeMove(int fromI, int fromJ, int toI, int toJ) {
+        if (lastFromI == -1) { // אם זה המהלך הראשון
+            return false;
+        }
+
+        // בודקים אם המהלך הנוכחי הפוך למהלך הקודם
+        return (fromI == lastToI && fromJ == lastToJ &&
+                toI == lastFromI && toJ == lastFromJ);
+    }
+
     private void movePoint(List<State> states, char[][] board, int i, int j, State curr) {
         int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; // למעלה, ימינה, למטה, שמאלה
-
         for (int[] direction : directions) {
             int newI = (i + direction[0] + SIZE) % SIZE;
             int newJ = (j + direction[1] + SIZE) % SIZE;
+
+            // בודקים אם זה מהלך הפוך
+            if (isOppositeMove(i, j, newI, newJ)) {
+                continue;
+            }
 
             if (board[newI][newJ] == '_') {
                 char[][] newBoard = copyBoard(board);
                 newBoard[newI][newJ] = board[i][j];
                 newBoard[i][j] = '_';
-
                 int costOfMove = getMoveCost(board[i][j]);
                 String newPath = curr.getPath() +
                         String.format("(%d,%d):%c:(%d,%d)--",
                                 i + 1, j + 1, board[i][j], newI + 1, newJ + 1);
+
+                // שומרים את המהלך הנוכחי כמהלך האחרון
+                lastFromI = i;
+                lastFromJ = j;
+                lastToI = newI;
+                lastToJ = newJ;
 
                 State newState = new State(newBoard, curr.getCost() + costOfMove, newPath, -1);
                 states.add(newState);
@@ -84,4 +106,3 @@ public class Board {
         return copyBoard(goalBoard);
     }
 }
-
